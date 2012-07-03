13d464c8e5b5282dd1792bd8a88b3ceb1ddf887d
Torte <Torte@Tortes-MacBook-Pro.local>
2012-07-04 01:56:49 +0700

    Initial Commit

---
+++ testtable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testtable
+//
+//  Created by Torte on 7/4/55 BE.
+//  Copyright (c) 2555 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testtable/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testtable
+//
+//  Created by Torte on 7/4/55 BE.
+//  Copyright (c) 2555 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
